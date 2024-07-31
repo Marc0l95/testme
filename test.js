498d9fb8e5ac8e9bf25b505ae450e22e5df4ebe3
MHF Lam <115367496+Marc0l95@users.noreply.github.com>
2024-07-31 15:32:53 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,69 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Navbar from './Navbar';
-import InputContainer from './InputContainer';
-import ValuesContainer from './ValuesContainer';
-import DetailedCalculations from './DetailedCalculations';
-import './MainApp.css';
+import React from 'react';
+import { formatToTwoDecimals } from './utils';
+import './DetailedCalculations.css';
 
-function MainApp() {
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [dropdown1, setDropdown1] = useState('');
-  const [dropdown2, setDropdown2] = useState('');
-  const [dropdown3, setDropdown3] = useState('');
-  const [result, setResult] = useState(null);
-  const [showDetailedCalculations, setShowDetailedCalculations] = useState(false);
+function DetailedCalculations({ data }) {
+  const renderTableData = (data) => {
+    if (!data) return null;
 
-  useEffect(() => {
-    const fetchValues = async () => {
-      const data = { input1, input2, dropdown1, dropdown2, dropdown3 };
-      try {
-        const response = await axios.post('http://localhost:5000/values', data);
-        setResult(response.data);
-      } catch (error) {
-        console.error('Error fetching values data:', error);
-      }
-    };
-
-    fetchValues();
-  }, [input1, input2, dropdown1, dropdown2, dropdown3]);
-
-  const toggleDetailedCalculations = () => {
-    setShowDetailedCalculations(!showDetailedCalculations);
+    return Object.keys(data).map((category, index) => (
+      <div key={index}>
+        <h2 className="category-header">{category}</h2>
+        {Object.keys(data[category]).map((product, productIndex) => {
+          const productData = data[category][product];
+          return (
+            <div key={productIndex}>
+              <h3>{product}</h3> {/* Product Heading */}
+              <table>
+                <thead>
+                  <tr>
+                    <th>Detail Key</th>
+                    <th>Detail Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(productData).map(([key, value], idx) => {
+                    if (value !== undefined && value !== null && value !== '') {
+                      return (
+                        <tr key={idx}>
+                          <td>{key}</td>
+                          <td>£{formatToTwoDecimals(value)}</td>
+                        </tr>
+                      );
+                    }
+                    return null; // Do not render if value is empty
+                  })}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    ));
   };
 
   return (
-    <div className="MainApp">
-      <Navbar />
-      <div className="toggle-container">
-        <button onClick={toggleDetailedCalculations} className="toggle-button">
-          {showDetailedCalculations ? 'Back to Calculator' : 'Show Detailed Calculations'}
-        </button>
-      </div>
-      <div className="container">
-        {!showDetailedCalculations ? (
-          <>
-            <InputContainer
-              input1={input1}
-              setInput1={setInput1}
-              input2={input2}
-              setInput2={setInput2}
-              dropdown1={dropdown1}
-              setDropdown1={setDropdown1}
-              dropdown2={dropdown2}
-              setDropdown2={setDropdown2}
-              dropdown3={dropdown3}
-              setDropdown3={setDropdown3}
-            />
-            <ValuesContainer data={result} />
-          </>
-        ) : (
-          <DetailedCalculations data={result} />
-        )}
-      </div>
+    <div className="detailed-calculations">
+      <h1>Detailed Calculations Breakdown</h1>
+      {renderTableData(data)}
     </div>
   );
 }
 
-export default MainApp;
+export default DetailedCalculations;
