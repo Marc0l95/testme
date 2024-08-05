db0b2a13d7573f9da8164b35de6115b21889c5d7
MHF Lam <115367496+Marc0l95@users.noreply.github.com>
2024-08-05 16:18:05 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
-const handleInputChange = (setter) => (e) => {
-    const value = e.target.value;
-    if (/^\d*$/.test(value)) { // Only allow digits
-      setter(value);
-      setError('');
-      const data = {
-        input1,
-        input2,
-        dropdown1,
-        dropdown2,
-        dropdown3,
-        [e.target.id]: value,
-      };
-      sendDataToBackend(data);
-    } else {
-      setError('Invalid input: Only non-negative digits are allowed.');
-    }
-  };
+// utils.js
+export const handleInputChange = (setter, setError, sendDataToBackend, state) => (e) => {
+  const value = e.target.value;
+  if (/^\d*$/.test(value)) { // Only allow digits
+    setter(value);
+    setError('');
+    const data = {
+      ...state,
+      [e.target.id]: value,
+    };
+    sendDataToBackend(data);
+  } else {
+    setError('Invalid input: Only non-negative digits are allowed.');
+  }
+};
